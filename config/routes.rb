b7bff85ff4fd6e0afbd8d4d3aa2c9f9b65ff49ca
Lovisa Svallingson <lovisasvallingson@gmail.com>
2016-02-16 11:16:01 -0700

    Add callback route and logout route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   get  '/dashboard', to: 'dashboard#show'
   root 'home#show'
+
+  # thanks to omniauth github gem
+  get '/auth/:provider/callback', to: 'sessions#create'
+
+  delete '/logout', to: 'sessions#destroy'
 end
